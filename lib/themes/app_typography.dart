57d6b330bba528647b6d276b3aad0115a0be2054
Thanapalan Vishaal <thanapalanvishaal@gmail.com>
2026-07-10 11:16:52 +0530

    AppTypography helper to replace google_fonts with locally bundled fonts

---
+++ lib/themes/app_typography.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class AppTypography {
+  static TextStyle orbitron({
+    Color? color,
+    double? fontSize,
+    FontWeight? fontWeight,
+    double? letterSpacing,
+    double? height,
+    FontStyle? fontStyle,
+    TextDecoration? decoration,
+  }) {
+    return TextStyle(
+      fontFamily: 'Orbitron',
+      color: color,
+      fontSize: fontSize,
+      fontWeight: fontWeight,
+      letterSpacing: letterSpacing,
+      height: height,
+      fontStyle: fontStyle,
+      decoration: decoration,
+    );
+  }
+
+  static TextStyle chakraPetch({
+    Color? color,
+    double? fontSize,
+    FontWeight? fontWeight,
+    double? letterSpacing,
+    double? height,
+    FontStyle? fontStyle,
+    TextDecoration? decoration,
+  }) {
+    return TextStyle(
+      fontFamily: 'ChakraPetch',
+      color: color,
+      fontSize: fontSize,
+      fontWeight: fontWeight,
+      letterSpacing: letterSpacing,
+      height: height,
+      fontStyle: fontStyle,
+      decoration: decoration,
+    );
+  }
+}
